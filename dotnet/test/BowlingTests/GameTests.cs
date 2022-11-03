c09472e0d3c6a9dbed139401b26a0b0d60d5c8a2
PORTE Damien <damien.porte@soprasteria.com>
2022-11-04 00:15:52 +0100

    Le score d'un tour est le nombre total de quilles renversées.

--- dotnet/test/BowlingTests/GameTests.cs
+++ dotnet/test/BowlingTests/GameTests.cs
@@ -1,27 +1,49 @@
 using FluentAssertions;
 using NUnit.Framework;
 using System;
+using System.Linq;
 
 namespace Bowling.Tests
 {
     [TestFixture]
     public class GameTests
     {
+        private Game _game;
+
+        [SetUp]
+        public void Setup()
+        {
+            _game = new Game();
+        }
+
         [Test]
         public void Given_an_empty_game_Then_score_Should_be_0()
         {
             // Arrange
-            Game game = new();
             var noPoint = Array.Empty<int>();
 
             // Act
-            // Nothing to do.
+            var score = _game.Score(noPoint);
 
             // Assert
-            game
-                .Score(noPoint)
+            score
                 .Should()
                 .Be(0);
+        }
+
+        [Test]
+        public void Given_a_game_with_one_lance_Then_score_Should_be_quille_count()
+        {
+            // Arrange
+            var oneLance = new int[] { 1 };
+
+            // Act
+            var score = _game.Score(oneLance);
+
+            // Assert
+            score
+                .Should()
+                .Be(oneLance.Sum());
         }
     }
 }
