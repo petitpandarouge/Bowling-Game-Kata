59f9d91fc5ab5acb537e595c73a6d81b19f825a7
PORTE Damien <damien.porte@soprasteria.com>
2022-11-04 01:30:56 +0100

    Un spare est quand le joueur renverse les 10 quilles en 2 lancés lors d'un tour.

---
+++ dotnet/src/Bowling/Tour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bowling
+{
+    public class Tour
+    {
+        private int _quillesFirstLance;
+        private int _quillesSecondLance;
+
+        public Tour(int quillesFirstLance, int quillesSecondLance)
+        {
+            _quillesFirstLance = quillesFirstLance;
+            _quillesSecondLance = quillesSecondLance;
+        }
+
+        public bool IsSpare()
+        {
+            return 
+                _quillesFirstLance < 10 && 
+                _quillesFirstLance + _quillesSecondLance == 10;
+        }
+    }
+}
